c37dca2e802936af363faaf423fe0b835062dfcc
edison <arlicle@gmail.com>
2020-01-10 22:16:22 +0800

    Fix 修正返回结果，返回一个对象

--- src/main.rs
+++ src/main.rs
@@ -37,76 +37,8 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     pretty_env_logger::init();
     let conf: Config = Config::from_args();
-    let s = "/aaa/{ddd}/dd";
-    let x = s.contains("{");
 
-
-    let mut x = "man\u{4e01}ana".to_string();
-//    let x = rand::random();
-    let y = "\u{9fbf}".bytes();
-    for i in y {
-        println!("i is {:?}", i);
-    }
-//    println!("chars {:?}", y);
-
-    println!("get_random_chinese_chars {}", utils::get_random_chinese_chars(100));
-
-    let s = "狻甘庘庾櫊暇锱掗曒隆";
-    let s = "矬鉩艌铂稢諼崏淈懙坍";
-    let a = '丁'.escape_unicode().to_string();
-    println!("aaa {:?}", a);
-    let mut rng = thread_rng();
-    println!("range is {} - {}",0x4e00,  0x9fa5);
-    let n: u32 = rng.gen_range(0x4e00, 0x9fa5);
-    let n: u32 = rng.gen_range(19968, 40869);
-    println!("n is {:?}", n);
-    let c = char::from_u32(0x4e00); // produces `Some(❤)`
-    println!("c is {:?}", c);
-    let n = format!("{:#x}", n);
-    let n = n.trim_start_matches("0x");
-    println!("n is {:?}", n);
-    let s = format!(r"\u{{{}}}", "4e00");
-    println!("s is {:?}", s);
-
-    println!("{}", x); // prints "mañana"
-
-    let heart: String = '❤'.escape_unicode().collect();
-
-    println!("heart: {}", heart);
-    println!("x is {:?}", x);
-    let res = ResourceDef::new(r"/post/{id:\d+}/");
-    let res2 = ResourceDef::new("/post/{id}/{ddd}/");
-    let y = res.is_match("/post/333/");
-//    let y = res.is_match("/post/aaa/");
-    let y2 = res2.is_match("/post/3/");
-    println!("y is {:?}", y);
-    println!("y2 is {:?}", y2);
-    println!("conf is {:?}", conf);
-//    let db = std::fs::read_to_string("data/model.json").expect(&format!("Unable to read file: {}", conf.folder));
-//    let data = Mutex::new(serde_json::from_str(&db).expect("Parse db file error"));
-    let db_file = String::from("api_docs/post.json");
-    let db = db::Database::load(&db_file);
-//    println!("db {:?}", db.lock().unwrap());
-    println!("Hello, world!");
-
-//    let counter = Arc::new(Mutex::new(0));
-//    let counter = Arc::new(0);
-//    let mut handles = vec![];
-//    for _ in 0..10 {
-//        let counter = Arc::clone(&counter);
-//        let handle = thread::spawn(move || {
-////            let mut c = counter.lock().unwrap();
-//            let mut c = counter;
-////            *c += 1;
-//            c += 1;
-//
-//            println!("counter is {:?}", c);
-//        });
-//        handles.push(handle);
-//    }
-//    for handle in handles {
-//        handle.join().unwrap();
-//    }
+    let db = db::Database::load();
 
     let web_db = web::Data::new(Mutex::new(db));
 
