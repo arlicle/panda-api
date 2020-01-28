025b1aabb50a4037172d435cd38de7b4e7e8b36b
edison <arlicle@gmail.com>
2020-01-28 17:49:20 +0800

    增加简单mock支持

--- src/mock/basic.rs
+++ src/mock/basic.rs
@@ -1,6 +1,7 @@
 use rand::{thread_rng, Rng};
 
-
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789)(*&^%$#@!~";
+const CHARSET2: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789)(*&^%$#@!~";
 
 /// 随机生成一个bool
 pub fn bool() -> bool {
@@ -15,5 +16,40 @@ pub fn bool() -> bool {
 }
 
 
+pub fn int() -> i64 {
+    let mut rng = thread_rng();
+    rng.gen::<i64>()
+}
+
+pub fn float() -> f64 {
+    let mut rng = thread_rng();
+    rng.gen::<f64>()
+}
 
 
+pub fn char() -> char {
+    let mut rng = thread_rng();
+    let idx = rng.gen_range(0, CHARSET.len());
+    CHARSET[idx] as char
+}
+
+
+/// 随机生成英文+符号的字符串
+pub fn string(mut length: usize) -> String {
+    let mut s = String::new();
+    let mut rng = thread_rng();
+
+    if length == 0 {
+        length = rng.gen_range(7, 20);
+    }
+
+    let l = CHARSET.len();
+    while length > 0 {
+//        let n: usize = rng.gen_range(0, l);
+//        let a1 = &CHARSET[n];
+        s.push(char());
+        length -= 1;
+    }
+
+    s
+}
