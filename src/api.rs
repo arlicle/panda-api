6c425a057ee697f998951f1e8d2c348ea134f692
edison <arlicle@gmail.com>
2020-01-31 11:30:42 +0800

    fix *

--- src/api.rs
+++ src/api.rs
@@ -450,8 +450,8 @@ fn is_websocket_connect(req: &HttpRequest) -> bool {
 pub fn get_field_type(field_attr: &Value) -> String {
     let field_type = match field_attr.get("type") {
         Some(v) => v.as_str().unwrap(),
-        None => if let Some(v) = field_attr.get("-name") {
-            "object"
+        None => if let Some(v) = field_attr.get("-type") {
+            v.as_str().unwrap()
         } else {
             if field_attr.is_array() {
                 "array"
@@ -861,11 +861,15 @@ pub fn create_mock_response(response_model: &Value) -> Map<String, Value> {
                                 "array" | _ => {
                                     let mut result2: Map<String, Value> = Map::new();
                                     result2.insert("key".to_string(), field_attr_one.clone());
-
+                                    println!("field_attr_one {:?}", field_attr_one);
+                                    println!("result2 {:?}", result2);
                                     let mut vec = Vec::with_capacity(length as usize);
                                     while length > 0 {
                                         let v = create_mock_response(&Value::Object({ &result2 }.clone()));
-                                        vec.push(v["key"].clone());
+                                        println!("v {:?}", v);
+                                        if v.contains_key("key") {
+                                            vec.push(v["key"].clone());
+                                        }
                                         length -= 1;
                                     }
 
